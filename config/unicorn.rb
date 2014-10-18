1eb3ef669d45fd0d8ec805f9c6662a044467586d
peeyush <peeyushagarwal1994@gmail.com>
2014-10-18 16:12:16 +0530

    Update config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,16 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/var/www/my-dropbox"
+working_directory "~/cs252-dropbox/my-dropbox"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/var/www/my-dropbox/pids/unicorn.pid"
+pid "~/cs252-dropbox/my-dropbox/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/my-dropbox/log/unicorn.log"
-stdout_path "/var/www/my-dropbox/log/unicorn.log"
+stderr_path "~/cs252-dropbox/my-dropbox/log/unicorn.log"
+stdout_path "~/cs252-dropbox/my-dropbox/log/unicorn.log"
 
 # Unicorn socket
 listen "/tmp/unicorn.[app name].sock"
